3c07e2b7141c5d358be4cbf56527cde8e6308eaf
Kovalenko Dmytro <kovalenko.dm80@gmail.com>
2024-08-23 08:33:29 +0300

    [+/*] Delete user feature added

--- src/main/java/com/gmail/apach/jenkins_demo/application/output/user/DeleteUserOutputPort.java
+++ src/main/java/com/gmail/apach/jenkins_demo/application/output/user/DeleteUserOutputPort.java
@@ -1,4 +1,6 @@
 package com.gmail.apach.jenkins_demo.application.output.user;
 
 public interface DeleteUserOutputPort {
+
+    void deleteByUserId(String userId);
 }
--- src/main/java/com/gmail/apach/jenkins_demo/domain/user/validator/DeleteUserPermissionsValidator.java
+++ src/main/java/com/gmail/apach/jenkins_demo/domain/user/validator/DeleteUserPermissionsValidator.java
@@ -3,7 +3,6 @@ package com.gmail.apach.jenkins_demo.domain.user.validator;
 import com.gmail.apach.jenkins_demo.common.constant.message.Error;
 import com.gmail.apach.jenkins_demo.common.exception.ForbiddenException;
 import com.gmail.apach.jenkins_demo.common.util.CurrentUserContextUtil;
-import com.gmail.apach.jenkins_demo.domain.common.constant.RoleType;
 import com.gmail.apach.jenkins_demo.domain.user.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
@@ -12,28 +11,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class GetUserByIdPermissionsValidator {
+public class DeleteUserPermissionsValidator {
 
     private final CurrentUserContextUtil currentUserContextUtil;
     private final MessageSource messageSource;
 
-    public void validate(User user) {
+    public void validate(User deletedUser) {
         final var currentUserContext = currentUserContextUtil.getContext();
 
-        final var isAdmin = currentUserContext.isAdmin();
-
-        final var managerTriesToGetAnyExceptAdmin =
-            currentUserContext.isManager()
-                && user.getRoles().stream().noneMatch(role -> role.getRole().equals(RoleType.ADMIN));
-
-        final var userTriesToGetSelf =
-            currentUserContext.isUser()
-                && user.getUsername().contentEquals(currentUserContext.username());
-
-        if (!(isAdmin || managerTriesToGetAnyExceptAdmin || userTriesToGetSelf)) {
+        if (!currentUserContext.isAdmin() || deletedUser.isAdmin()) {
             throw new ForbiddenException(messageSource.getMessage(
-                Error.FORBIDDEN_USER_GET_BY_ID.getKey(),
-                new Object[]{user.getUserId()},
+                Error.FORBIDDEN_USER_DELETE_BY_ID.getKey(),
+                new Object[]{deletedUser.getUserId()},
                 LocaleContextHolder.getLocale()));
         }
     }
