8026b561cb15b5ce32956bd5c6fe6860cc870eb4
Kovalenko Dmytro <kovalenko.dm80@gmail.com>
2024-08-26 11:34:03 +0300

    [+/*] Get users feature added

--- src/main/java/com/gmail/apach/jenkins_demo/common/constant/message/Error.java
+++ src/main/java/com/gmail/apach/jenkins_demo/common/constant/message/Error.java
@@ -20,6 +20,7 @@ public enum Error {
     FORBIDDEN_AUTHORITIES_NOT_FOUND("error.forbidden.authorities.not.found"),
     FORBIDDEN_USER_CREATION("error.forbidden.user.creation"),
     FORBIDDEN_USER_GET_BY_ID("error.forbidden.user.get.id"),
+    FORBIDDEN_USER_GET_LIST("error.forbidden.user.get.list"),
     FORBIDDEN_USER_UPDATE_BY_ID("error.forbidden.user.update.id"),
     FORBIDDEN_USER_UPDATE_ENABLED("error.forbidden.user.update.enabled"),
     FORBIDDEN_USER_UPDATE_ROLES("error.forbidden.user.update.roles"),
